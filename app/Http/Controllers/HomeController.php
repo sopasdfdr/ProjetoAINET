c846b9ad50fc09585978fa9cc660f254443c2a1f
Diogo Rodrigues <diogosopasrodrigues@gmail.com>
2020-05-06 18:23:07 +0100

    f3f33

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\User;
+use App\Movimento;
+use App\Conta;
 
 class HomeController extends Controller
 {
     public function index(){
-        $users = DB::table('users')->count();
-        $contas = DB::table('contas')->where('deleted_at', NULL)->count();
-        $movimentos = Db::table('movimentos')->count();
+        $users = User::count();
+        $contas = Conta::where('deleted_at', NULL)->count();
+        $movimentos = Movimento::count();
 
         return view('home.index')
         ->withUsers($users)
