a6bc659212bb333e8499c0d6b219d77bf7ccf4e7
Miguel Ferreira <miguelbernardinoferreira@gmail.com>
2020-05-09 22:12:12 +0100

    Filtros F3 concluidos!

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,22 +9,30 @@ class AdminController extends Controller
 {
     public function index(Request $request)
     {
-        $nome = $request->nome;
-        $email = $request->email;
-        $blq = $request->blq;
-        $adm = $request->adm;
+        $nome = $request->nome ?? NULL;
+        $email = $request->email ?? NULL;
+        $adm = $request->adm ?? 3;
+        $blq = $request->blq ?? 3;
 
         $query = User::query()->select('name' , 'email', 'foto', 'adm', 'bloqueado');
 
-        if($nome)
+        if($nome!= NULL)
         {
             $query->where('name', 'LIKE', '%' .$nome. '%');
         }
-        if($email)
+        if($email != NULL)
         {
-            $query->where('email',$email);
+            $query->where('email', 'LIKE', '%' .$email. '%');
         }
+        if($adm != 3){
+            $query->where('adm', $adm);
+        }
+        if($blq != 3)
+        {
+            $query->where('bloqueado', $blq);
+        }
+
         $users = $query->paginate(10);
-        return view('admin.index')->withUsers($users);
+        return view('admin.index')->withUsers($users)->withNome($nome)->withEmail($email)->withAdm($adm)->withBlq($blq);
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,36 +4,70 @@
 @section('content')
 
 <form class="user-search" action="#" method="GET">
-    <div class="form-row align-items-center">
+    <div class="row align-items-center">
         <div class="col-3">
             <label for="userSearch">Nome:</label>
         </div>
         <div class="col-4">
-            <label for="inlineFormInputGroup">Email:</label>
+            <label for="mailSearch">Email:</label>
+        </div>
+        <div class="col-2">
+            <label for="searchAdmin">Tipo:</label>
+        </div>
+        <div class="col-2">
+            <label for="searchBlq">Bloqueado:</label>
         </div>
     </div>
-    <div class="form-row align-items-center">
+    <div class="row align-items-center">
         <div class="col-3">
-            <input type="text" class="form-control" id="userSearch" name="nome" placeholder="Nome a filtrar">
+            <input type="text" class="form-control" id="userSearch" name="nome" placeholder="Nome a filtrar" value={{$nome}}>
         </div>
         <div class="col-4">
             <div class="input-group">
               <div class="input-group-prepend">
                 <div class="input-group-text">@</div>
               </div>
-              <input type="email" class="form-control" id="mailSearch" name="email" placeholder="Email a filtrar">
+            <input type="text" class="form-control" id="mailSearch" name="email" placeholder="Email a filtrar" value={{$email}}>
             </div>
         </div>
-        <div class="form-check form-check-inline">
-            <input type="checkbox" class="form-check-input" id="searchAdmin" name="adm" value="1" {{$adm ?? '' == 1 ? 'checked' : ''}}>
-            <label class="form-check-label" for="searchAdmin">Admin</label>
+
+       <!-- <select class="form-control col-1" id="searchAdmin" name="adm">
+            <option value=NULL $adm ?? '' == NULL ? 'selected' : '' >Todos</option>
+            <option value=0 $adm ?? '' == 0 ? 'selected' : '' >Normal</option>
+            <option value=1 $adm ?? '' == 1 ? 'selected' : '' >Admin</option>
+        </select>-->
+
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="searchAdmin" name="adm" class="custom-control-input" value=3 {{$adm == 3 ? 'checked' : ''}}>
+            <label class="custom-control-label" for="searchAdmin">Todos</label>
         </div>
-        <div class="form-check form-check-inline">
-            <input type="checkbox" class="form-check-input" id="searchBloqueado" name="blq" value="1" {{$blq ?? '' == 1 ? 'checked' : ''}}>
-            <label class="form-check-label" for="searchBloqueado">Bloqueado</label>
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="searchAdmin1" name="adm" class="custom-control-input" value=1 {{$adm == 1 ? 'checked' : ''}}>
+            <label class="custom-control-label" for="searchAdmin1">Admin</label>
         </div>
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="searchAdmin2" name="adm" class="custom-control-input" value=0 {{$adm == 0 ? 'checked' : ''}}>
+            <label class="custom-control-label" for="searchAdmin2">Normal</label>
+        </div>
+
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="searchBlq" name="blq" class="custom-control-input" value=3 {{$blq == 3 ? 'checked' : ''}}>
+            <label class="custom-control-label" for="searchBlq">Todos</label>
+        </div>
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="searchBlq1" name="blq" class="custom-control-input" value=1 {{$blq == 1 ? 'checked' : ''}}>
+            <label class="custom-control-label" for="searchBlq1">Sim</label>
+        </div>
+        <div class="custom-control custom-radio custom-control-inline">
+            <input type="radio" id="searchBlq2" name="blq" class="custom-control-input" value=0 {{$blq == 0 ? 'checked' : ''}}>
+            <label class="custom-control-label" for="searchBlq2">Não</label>
+        </div>
+
         <div class="col-auto">
             <button type="submit" class="btn btn-primary" id="btn-filter">Filtrar</button>
+        </div>
+        <div class="col-auto">
+            <a class="btn btn-primary" href="/admin">Clean</a>
         </div>
     </div>
 </form>
